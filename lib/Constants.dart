788e479ca2dbd402690b7105ec95d3f5fa4aa839
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-07-10 23:54:33 +0530

    build : Add Constants.dart

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 
 const AccentColor=Color(0xFFF16823);
 const TextFillColor=Colors.grey;
+const ActiveTextSliderColor=Colors.white;
+const InactiveTextSliderColor=Colors.black;
+const ActiveCardColor=Color(0xFFF16823);
+const InactiveCardColor=Colors.white;
