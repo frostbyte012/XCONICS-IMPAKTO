e2550804966f77e05852ecf4b68be8acc1c61fd1
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-07-11 23:26:04 +0530

    build : Add lib/Constants.dart

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -6,3 +6,5 @@ const ActiveTextSliderColor=Colors.white;
 const InactiveTextSliderColor=Colors.black;
 const ActiveCardColor=Color(0xFFF16823);
 const InactiveCardColor=Colors.white;
+const InactiveBottomNavigationActiveColor=Colors.grey;
+const ActiveBottomNavigationActiveColor=Color(0xFFF16823);
