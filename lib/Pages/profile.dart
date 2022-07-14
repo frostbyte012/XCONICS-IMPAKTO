ecf60eaa7e1a9072be453e341ad740b1cb4b9edf
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-07-14 12:30:58 +0530

    lib/Pages/profile.dart

---
+++ lib/Pages/profile.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:xconics_app_custom_widget_pages_coponents_library/Constants.dart';
+import 'Profile_Edit_Page.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text("Profile"),
+        backgroundColor: AccentColor,
+      ),
+      body: ListView(
+
+        children: [
+
+          Container(
+
+            child: Container(
+              color: AccentColor,
+              height: 250,
+              width: double.infinity,
+              child: Column(
+
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+
+                children: [
+
+                  CircleAvatar(
+                    radius: 90,
+                    backgroundColor: Colors.grey,
+                    child: Icon(Icons.person,color: Colors.white,size:40,),
+                  ),
+
+                  SizedBox(height: 20,),
+
+                  Text("Deepraj Majumdar",style: TextStyle(color: Colors.white,fontSize: 20),),
+                ],
+              ),
+            ),
+
+          ),
+
+
+          SizedBox(height: 20,),
+
+          ListTile(
+            leading:Icon(Icons.person),
+            title:Row(children: [Text("Name : "),Text(" Deepraj Majumdar"),],),
+          ),
+
+          ListTile(
+            leading:Icon(Icons.important_devices),
+            title:Row(children: [Text("User ID : "),Text(" froatbyte012"),],),
+          ),
+
+          ListTile(
+            leading:Icon(Icons.monitor_weight),
+            title:Row(children: [Text("Weight : "),Text(" 50 KG"),],),
+          ),
+
+          ListTile(
+            leading:Icon(Icons.height),
+            title:Row(children: [Text("Height : "),Text(" 5.7 FT"),],),
+          ),
+
+          ListTile(
+            leading:Icon(Icons.man),
+            title:Row(children: [Text("Gender : "),Text(" Male"),],),
+          ),
+
+
+        ],
+
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.edit),
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>ProfileEditPage(uid: "frostbyte012")));
+        },
+        backgroundColor: AccentColor,
+      ),
+
+    );
+  }
+}
