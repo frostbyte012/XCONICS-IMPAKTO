8273830d3de2cbdbcbc3d8c817d7b3098a663ebd
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-08-08 23:32:01 +0530

    fix : Update and call Firebase initializeapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'Pages/InputPage.dart';
 import 'Pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main()=>runApp(MyApp(),);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
