27200288b9bc9cd9a37df70193c43138e634ea1f
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-08-13 21:51:54 +0530

    Update lib/Constants.dart

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
 
 const AccentColor=Color(0xFFF16823);
 const TextFillColor=Colors.grey;
@@ -8,3 +10,7 @@ const ActiveCardColor=Color(0xFFF16823);
 const InactiveCardColor=Colors.white;
 const InactiveBottomNavigationActiveColor=Colors.grey;
 const ActiveBottomNavigationActiveColor=Color(0xFFF16823);
+const kDefaultColour=Color.fromARGB(255,116,238,21);
+const kAppBarTitleTextStyle=TextStyle(color:kDefaultColour,fontWeight: FontWeight.bold,fontSize:18);
+const kQRButtonTextStyle=TextStyle(color:kDefaultColour,fontWeight: FontWeight.bold,fontSize:17);
+const kQRGeneratorButtonTextStyle=TextStyle(color:kDefaultColour,fontWeight: FontWeight.bold,fontSize:25);
